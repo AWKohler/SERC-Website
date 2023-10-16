3705da97873699583f52174cefb1f9fd6bc072ac
AWKohler <aronne336@icloud.com>
2023-10-16 18:28:54 -0400

    progress

--- app/page.js
+++ app/page.js
@@ -2,10 +2,56 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-          Welcome to the website
-      </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-700">
+    {/*<main className="flex min-h-screen flex-col items-center justify-between p-24">*/}
+      {/*/!*<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">*!/*/}
+      {/*<div className="z-10 max-w-5xl w-full items-center justify-between font-mono lg:flex text-9xl stroke-amber-100 stroke-2 -webkit-text-stroke">*/}
+      {/*    Welcome to the website*/}
+      {/*</div>*/}
+
+        {/*<div className="outline-title text-white pb-2 text-9xl font-bold text-center mb-12 mt-8">*/}
+        {/*    Welcome to the website*/}
+        {/*</div>*/}
+        {/*<div className="tfixed text-white pb-2 text-9xl font-bold text-center mb-12 mt-8">*/}
+        {/*<div className="tfixed pb-2 text-9xl font-bold text-center mb-12 mt-8">*/}
+        {/*    SERC*/}
+        {/*</div>*/}
+
+
+        {/*<div className="text-white pb-2 text-9xl font-bold text-center mb-12 mt-8 tracking-tighter">*/}
+        {/*<div className="text-white text-[9rem] font-bold italic text-center tracking-tighter">*/}
+        {/*    SERC*/}
+        {/*</div>*/}
+
+        {/*<div className="tfixed text-[9rem] font-bold italic text-center tracking-tighter">*/}
+        {/*    SERC*/}
+        {/*</div>*/}
+
+
+        {/*<div className="tfixed text-[9rem] font-bold absolute italic text-center tracking-tighter mt-28">*/}
+        {/*    SERC*/}
+        {/*</div>*/}
+        {/*<div className="text-white text-[9rem] font-bold absolute italic text-center tracking-tighter mt-28">*/}
+        {/*    SERC*/}
+        {/*</div>*/}
+
+        <div className={"flex flex-col text-[11rem] tracking-tighter"}>
+            <div className="text-white absolute font-bold italic text-center z-10">
+                SERC
+            </div>
+
+            <div className={"flex flex-col absolute justify-center items-center text-center font-bold italic mt-28"}>
+                <div className="tfixed">
+                    SERC
+                </div>
+                <div className="text-gray-700 absolute">
+                    SERC
+                </div>
+            </div>
+
+
+        </div>
+
     </main>
   )
 }
