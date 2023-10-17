fe5de852bf21f213dcde856f0866f118757f41d5
AWKohler <aronne336@icloud.com>
2023-10-16 20:22:33 -0400

    m

--- app/page.js
+++ app/page.js
@@ -1,56 +1,100 @@
 import Image from 'next/image'
+// import bgImage from '/gradient.png';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-gray-700">
-    {/*<main className="flex min-h-screen flex-col items-center justify-between p-24">*/}
-      {/*/!*<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">*!/*/}
-      {/*<div className="z-10 max-w-5xl w-full items-center justify-between font-mono lg:flex text-9xl stroke-amber-100 stroke-2 -webkit-text-stroke">*/}
-      {/*    Welcome to the website*/}
-      {/*</div>*/}
+    // <main className="flex flex-col items-center justify-center p-24 bg-gray-700">
+    <main className="flex flex-col items-center justify-center bg-primary">
 
-        {/*<div className="outline-title text-white pb-2 text-9xl font-bold text-center mb-12 mt-8">*/}
-        {/*    Welcome to the website*/}
-        {/*</div>*/}
-        {/*<div className="tfixed text-white pb-2 text-9xl font-bold text-center mb-12 mt-8">*/}
-        {/*<div className="tfixed pb-2 text-9xl font-bold text-center mb-12 mt-8">*/}
-        {/*    SERC*/}
-        {/*</div>*/}
+        {/*<div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>*/}
+        <div className="hero min-h-screen" style={{ backgroundImage: `url("/gradient.png")` }}>
+        {/*<div className="hero min-h-screen" style={{backgroundImage: bgImage}}>*/}
+        {/*    <div className="hero-overlay bg-opacity-60"></div>*/}
+        {/*    <div className="hero-content text-center text-neutral-content">*/}
+        {/*        <div className="max-w-md">*/}
+        {/*            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>*/}
+        {/*            <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>*/}
+        {/*            <button className="btn btn-primary">Get Started</button>*/}
+        {/*        </div>*/}
+        {/*    </div>*/}
+
+            {/*<div className={"hero-content bg-[#00ffffffff] p-4 pr-16 "}>*/}
+            <div className={"hero-content bg-[#00ffffffff] p-4 pr-16 flex flex-col items-center justify-center"}>
+                {/*<div className={"flex flex-col text-[11rem] italic tracking-tighter"}>*/}
+                <div className={"flex flex-col text-[11rem] italic tracking-tighter items-center justify-center"}>
+                    {/*<div className="text-white absolute font-bold text-center z-10">*/}
+                    <div className="text-white font-bold text-center z-10">
+                        SERC
+                    </div>
+
+                    {/*<div className={"flex flex-col justify-center items-center text-center font-bold mt-28"}>*/}
+                    {/*    <div className="tfixed">*/}
+                    {/*        SERC*/}
+                    {/*    </div>*/}
+                    {/*    /!*<div className="text-gray-700 absolute">*!/*/}
+                    {/*    <div className="bg-[#00ffffffff] absolute">*/}
+                    {/*        SERC*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
 
 
-        {/*<div className="text-white pb-2 text-9xl font-bold text-center mb-12 mt-8 tracking-tighter">*/}
-        {/*<div className="text-white text-[9rem] font-bold italic text-center tracking-tighter">*/}
-        {/*    SERC*/}
-        {/*</div>*/}
 
-        {/*<div className="tfixed text-[9rem] font-bold italic text-center tracking-tighter">*/}
-        {/*    SERC*/}
-        {/*</div>*/}
+                    <div className={"ml-32 mt-72"}>
+                        <svg width="600" xmlns="http://www.w3.org/2000/svg">
+                            <text y="135" fill="none" stroke="white" stroke-width="3" font-weight="800">
+                                SERC
+                            </text>
+                        </svg>
+                    </div>
+
+                    {/*<svg width="600" xmlns="http://www.w3.org/2000/svg">*/}
+                    {/*    <text y="135" fill="none" stroke="white" stroke-width="3" font-weight="800">*/}
+                    {/*        SERC*/}
+                    {/*    </text>*/}
+                    {/*</svg>*/}
 
 
-        {/*<div className="tfixed text-[9rem] font-bold absolute italic text-center tracking-tighter mt-28">*/}
-        {/*    SERC*/}
-        {/*</div>*/}
-        {/*<div className="text-white text-[9rem] font-bold absolute italic text-center tracking-tighter mt-28">*/}
-        {/*    SERC*/}
-        {/*</div>*/}
+                    {/*<svg width="308" height="104" viewBox="0 0 308 104" xmlns="http://www.w3.org/2000/svg">*/}
+                    {/*    <text x="0" y="120" fill="none" stroke="white" stroke-width="3" font-weight="800">SERC</text>*/}
+                    {/*</svg>*/}
 
-        <div className={"flex flex-col text-[11rem] tracking-tighter"}>
-            <div className="text-white absolute font-bold italic text-center z-10">
-                SERC
-            </div>
-
-            <div className={"flex flex-col absolute justify-center items-center text-center font-bold italic mt-28"}>
-                <div className="tfixed">
-                    SERC
                 </div>
-                <div className="text-gray-700 absolute">
-                    SERC
-                </div>
             </div>
-
-
         </div>
+
+        {/*<div className={"bg-red-500 p-4 pr-16"}>*/}
+        {/*    <div className={"flex flex-col text-[11rem] italic tracking-tighter"}>*/}
+        {/*        <div className="text-white absolute font-bold text-center z-10">*/}
+        {/*            SERC*/}
+        {/*        </div>*/}
+
+        {/*        <div className={"flex flex-col justify-center items-center text-center font-bold mt-28"}>*/}
+        {/*            <div className="tfixed">*/}
+        {/*                SERC*/}
+        {/*            </div>*/}
+        {/*            /!*<div className="text-gray-700 absolute">*!/*/}
+        {/*            <div className="text-red-500 absolute">*/}
+        {/*                SERC*/}
+        {/*            </div>*/}
+        {/*        </div>*/}
+
+        {/*        /!*<div className={"z-10"}>*!/*/}
+        {/*        /!*    <svg width="308" height="104" viewBox="0 0 308 104" xmlns="http://www.w3.org/2000/svg">*!/*/}
+        {/*        /!*        <text x="0" y="90" fill="none" stroke="white" stroke-width="3" font-weight="800">2022</text>*!/*/}
+        {/*        /!*    </svg>*!/*/}
+        {/*        /!*</div>*!/*/}
+        {/*    </div>*/}
+
+            {/*<svg width="308" height="104" viewBox="0 0 308 104" xmlns="http://www.w3.org/2000/svg">*/}
+            {/*    /!*<text x="0" y="90" fill="none" stroke="white" stroke-width="3" font-family="Noto Sans" font-size="100px" font-weight="800">2022</text>*!/*/}
+            {/*    <text x="0" y="90" fill="none" stroke="white" stroke-width="3" font-weight="800">2022</text>*/}
+            {/*</svg>*/}
+
+        {/*</div>*/}
+
+
+
+
 
     </main>
   )
