d72e8bdb5b4bbe3f1020535d80c4f1faa485b0e7
AWKohler <aronne336@icloud.com>
2023-10-17 16:16:47 -0400

    simple footer

--- app/page.js
+++ app/page.js
@@ -1,39 +1,14 @@
 import Image from 'next/image'
 import Hero from "@/components/hero";
+import Navbar from "@/components/navbar";
 // import bgImage from '/gradient.png';
 
 export default function Home() {
   return (
       <main className="flex min-h-screen w-full flex-col items-center bg-blue-500">
 
-          <div className="navbar bg-base-100">
-              <div className="navbar-start">
-                  <div className="dropdown">
-                      <label tabIndex={0} className="btn btn-ghost btn-circle">
-                          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /></svg>
-                      </label>
-                      <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
-                          <li><a>Homepage</a></li>
-                          <li><a>Portfolio</a></li>
-                          <li><a>About</a></li>
-                      </ul>
-                  </div>
-              </div>
-              <div className="navbar-center">
-                  <a className="btn btn-ghost normal-case text-xl">Software engineering readiness club</a>
-              </div>
-              <div className="navbar-end">
-                  <button className="btn btn-ghost btn-circle">
-                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
-                  </button>
-                  <button className="btn btn-ghost btn-circle">
-                      <div className="indicator">
-                          <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
-                          <span className="badge badge-xs badge-primary indicator-item"></span>
-                      </div>
-                  </button>
-              </div>
-          </div>
+
+          <Navbar></Navbar>
 
           <div className="w-full">
 
@@ -56,7 +31,7 @@ export default function Home() {
                                   </h1>
 
                                   <p>
-                                      This is a person who is in the club
+                                      This is a person who is in da club
                                   </p>
                               </div>
 
@@ -77,7 +52,7 @@ export default function Home() {
                                   </h1>
 
                                   <p>
-                                      This is a person who is in the club
+                                      This is a person who is in da club
                                   </p>
                               </div>
                           </div>
@@ -97,7 +72,7 @@ export default function Home() {
                                   </h1>
 
                                   <p>
-                                      This is a person who is in the club
+                                      This is a person who is in da club
                                   </p>
                               </div>
                           </div>
@@ -107,6 +82,21 @@ export default function Home() {
 
 
 
+              </div>
+          </div>
+
+          <div className={"w-full h-96 bg-base-100 flex p-16 px-24 items-center"}>
+              <div className="w-60 rounded">
+                  <img src="Logo.png" />
+              </div>
+              <div className={"ml-12 h-full py-8 prose lg:prose-md"}>
+                  <h1>
+                      SER Club
+                  </h1>
+
+                  <p>
+                      This is a cool club
+                  </p>
               </div>
           </div>
 
--- components/hero.js
+++ components/hero.js
@@ -4,9 +4,40 @@ export default function Hero() {
         // <div className="hero h-full" style={{ backgroundImage: `url("/gradient2.png")` }}>
         // <div className="hero h-full py-32" style={{ backgroundImage: `url("/gradient2.png")` }}>
         <div className="hero h-full py-32" style={{ backgroundImage: `url("/daisys.jpg")` }}>
+            {/*<div className="hero-overlay bg-opacity-60 h-full"></div>*/}
 
             {/*<div className={"hero-content bg-[#00ffffffff] flex flex-col items-center justify-center border-4 border-white overflow-clip"}>*/}
-            <div className={"hero-content flex flex-col items-center justify-center border-4 border-white overflow-clip"}>
+            {/*<div className={"hero-content flex flex-col items-center justify-center border-4 border-white overflow-clip"}>*/}
+                <div className={"flex flex-col text-[15rem] italic tracking-tighter items-center justify-center -ml-10 -mt-20 py-5 scroll-pr-3.5"}>
+                    <div className="text-white font-bold text-center z-10">
+                        SERC
+                    </div>
+
+                    <div className={"ml-6 -mt-14"}>
+                        <svg width="628" height={"184"} xmlns="http://www.w3.org/2000/svg">
+                            <text x={"-5"} y="179" fill="none" stroke="white" stroke-width="3" font-weight="800">
+                                SERC
+                            </text>
+                        </svg>
+                    </div>
+
+                </div>
+            {/*</div>*/}
+
+            {/*<div className="mockup-code">*/}
+            {/*    <pre data-prefix="$"><code>npm i daisyui</code></pre>*/}
+            {/*    <pre data-prefix=">" className="text-warning"><code>installing...</code></pre>*/}
+            {/*    <pre data-prefix=">" className="text-success"><code>Done!</code></pre>*/}
+            {/*</div>*/}
+
+        </div>
+    )
+}
+
+
+
+/*
+<div className={"hero-content flex flex-col items-center justify-center border-4 border-white overflow-clip"}>
                 <div className={"flex flex-col text-[15rem] italic tracking-tighter items-center justify-center -ml-10 -mt-20 py-5 scroll-pr-3.5"}>
                     <div className="text-white font-bold text-center z-10">
                         SERC
@@ -22,13 +53,4 @@ export default function Hero() {
 
                 </div>
             </div>
-
-            {/*<div className="mockup-code">*/}
-            {/*    <pre data-prefix="$"><code>npm i daisyui</code></pre>*/}
-            {/*    <pre data-prefix=">" className="text-warning"><code>installing...</code></pre>*/}
-            {/*    <pre data-prefix=">" className="text-success"><code>Done!</code></pre>*/}
-            {/*</div>*/}
-
-        </div>
-    )
-}
+ */
