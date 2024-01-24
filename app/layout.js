c6567ae8c58fc6f4feefa1948ebc59d2ad2c505d
AWKohler <aronne336@icloud.com>
2024-01-24 18:07:57 -0500

    ok

--- app/layout.js
+++ app/layout.js
@@ -9,12 +9,34 @@ export const metadata = {
   description: 'Club website',
 }
 
+// script.setAttribute('data-id', "embedai-Qclufpg5");
+
+
 export default function RootLayout({ children }) {
   return (
       <html lang="en">
           <body className={inter.className}>
               <Navbar/>
               {children}
+
+              {/*<script type="application/javascript" dangerouslySetInnerHTML={{ __html: `function loadScript() { var head = document.getElementsByTagName('head')[0]; var script = document.createElement('script'); script.type = 'text/javascript'; script.src = "https://test-resources.vercel.app/embedchat.js"; script.setAttribute('data-id', "clqcmst850003umgxmzets2ou"); script.id = "embedchat"; head.appendChild(script); } if (!location.href.includes("ai-demo-my-chatbot")) { loadScript(); }`}}>*/}
+              {/*</script>*/}
+
+              <script type="application/javascript" dangerouslySetInnerHTML={{ __html: `
+              	function loadScript() {
+              	  var head = document.getElementsByTagName('head')[0];
+              	  var script = document.createElement('script');
+              	  script.type = 'text/javascript';
+              	  script.src = "https://test-resources.vercel.app/embedchat.js";
+              	  script.setAttribute('data-id', "clqcmst850003umgxmzets2ou");
+              	  script.id = "embedchat";
+              	  head.appendChild(script);
+              	}
+              	if (!location.href.includes("ai-demo-my-chatbot")) {
+              	  loadScript();
+              	}
+                `}}>
+              </script>
           </body>
       </html>
   )
